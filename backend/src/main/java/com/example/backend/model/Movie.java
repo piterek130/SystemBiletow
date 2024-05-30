7ea076fe20f77525399ebb59a975c583be3011b0
ewakotas <ewakotas01@gmail.com>
2024-05-30 14:24:11 +0200

    Remove unnecessary files

--- backend/src/main/java/com/example/backend/model/Movie.java
+++ backend/src/main/java/com/example/backend/model/Movie.java
@@ -1,2 +1,30 @@
-package com.example.backend.model;public class Movie {
+package com.example.backend.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Movie {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+  private String description;
+  private int duration;
+  private String imageUrl;
+  private String trailerUrl;
+  private double price;
+  private String releaseDate;
+  private String production;
+  private String director;
+
 }
--- backend/src/main/java/com/example/backend/service/MovieService.java
+++ backend/src/main/java/com/example/backend/service/MovieService.java
@@ -1,2 +1,23 @@
-package com.example.backend.service;public class MovieService {
+package com.example.backend.service;
+
+import com.example.backend.model.Movie;
+import com.example.backend.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+  @Autowired
+  private MovieRepository movieRepository;
+
+  public List<Movie> getAllMovies() {
+    return movieRepository.findAll();
+  }
+
+  public Movie getMovieById(Long id) {
+    return movieRepository.findById(id).orElse(null);
+  }
 }
